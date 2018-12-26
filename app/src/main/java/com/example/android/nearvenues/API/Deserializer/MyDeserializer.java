273cab6810b01bf6062ab9fb88e8cc5bac6763f2
almiavicas <almiavicas@gmail.com>
2018-12-26 15:26:29 -0400

    Deleted Debugging code

--- app/src/main/java/com/example/android/nearvenues/API/Deserializer/MyDeserializer.java
+++ app/src/main/java/com/example/android/nearvenues/API/Deserializer/MyDeserializer.java
@@ -1,5 +1,7 @@
 package com.example.android.nearvenues.API.Deserializer;
 
+import android.util.Log;
+
 import com.example.android.nearvenues.models.Venue;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
@@ -19,12 +21,15 @@ public class MyDeserializer implements JsonDeserializer<List<Venue>> {
     public List<Venue> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         List<Venue> venuesList = new ArrayList<>();
         JsonArray venues = json.getAsJsonObject().getAsJsonObject("response").getAsJsonArray("venues");
-        Iterator<JsonElement> it =  venues.iterator();
+        Iterator<JsonElement> it = venues.iterator();
         do {
             JsonObject obj = it.next().getAsJsonObject();
             String name = obj.get("name").getAsString();
-            int distance = obj.getAsJsonObject("location").get("distance").getAsInt();
-            String address = obj.getAsJsonObject("location").get("address").getAsString();
+
+            obj = obj.getAsJsonObject("location");
+
+            int distance = obj.get("distance").getAsInt();
+            String address = obj.has("address") ? obj.get("address").getAsString() : "";
             venuesList.add(new Venue(name, address, distance));
         } while (it.hasNext());
 
